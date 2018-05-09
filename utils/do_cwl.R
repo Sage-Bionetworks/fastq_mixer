2cd2443b313811427e560cd51f4ac86799433593
Ubuntu <ubuntu@ip-172-31-18-244.ec2.internal>
2018-05-09 01:45:09 +0000

    cores = inout yamls

--- utils/do_cwl.R
+++ utils/do_cwl.R
@@ -3,10 +3,10 @@ library(plyr)
 library(purrr)
 library(stringr)
 
-doMC::registerDoMC(cores = detectCores() -1)
-
 yamls <- list.files() %>% 
     purrr::keep(str_detect(., ".yaml$")) 
+
+doMC::registerDoMC(cores = length(yamls))
 
 log_files <- stringr::str_replace(yamls, "yaml", "log")
 
@@ -19,4 +19,4 @@ logs <- plyr::llply(
             stderr = T),
     .parallel = T)
 
-purr::walk2(logs, log_files, writeLines)
+purrr::walk2(logs, log_files, writeLines)
