86ab2d5da71216f456f66b348c20f0eb0ac63d3a
andrewelamb <andrewelamb@gmail.com>
2018-05-09 09:42:45 -0700

    changed to not parallel

--- utils/do_cwl.R
+++ utils/do_cwl.R
@@ -1,22 +1,19 @@
-library(doMC)
-library(plyr)
 library(purrr)
 library(stringr)
-
-doMC::registerDoMC(cores = detectCores() -1)
 
 yamls <- list.files() %>% 
     purrr::keep(str_detect(., ".yaml$")) 
 
-log_files <- stringr::str_replace(yamls, "yaml", "log")
+logs <- stringr::str_replace(yamls, "yaml", "log")
 
-logs <- plyr::llply(
-    yamls, 
-    function(yaml) 
-        system2(
-            "cwltool", 
-            args = c("fastq_mixer/fastq_mixer.cwl", yaml), 
-            stderr = T),
-    .parallel = T)
+do_cwl <- function(yaml, log){
+    stderr <- system2(
+        "cwltool", 
+        args = c("fastq_mixer/fastq_mixer.cwl", yaml), 
+        stderr = T)
+    writeLines(stderr, log)
+}
 
-purr::walk2(logs, log_files, writeLines)
+purrr::walk2(yamls, logs, do_cwl)
+
+
